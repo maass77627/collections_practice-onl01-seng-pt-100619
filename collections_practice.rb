0fbae252f6aff1f6e26be12ae58cb7d3a7438def
kelly r maass <kmaass77627@gmail.com>
2019-10-21 02:12:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,17 @@
-sort_array_asc(array)
+def sort_array_asc(array)
   array.sort
  end 
+ 
+ def sort_array_desc(array)
+   array.sort do |a,b|
+     b <=> a
+  end 
+end 
+
+def sort_array_char_count(array)
+  array.sort_by {|string| string.length}
+end 
+ 
+ 
 
 
