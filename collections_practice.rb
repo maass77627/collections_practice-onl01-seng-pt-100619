272e2da7e5dad4ca61d3c87dfe1342f47b43c7ed
kelly r maass <kmaass77627@gmail.com>
2019-10-21 02:24:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,6 +11,32 @@ end
 def sort_array_char_count(array)
   array.sort_by {|string| string.length}
 end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array 
+end 
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |element|
+    element[0].downcase == "a"
+  end 
+end 
+
+
+  
+  
+  
  
  
 
